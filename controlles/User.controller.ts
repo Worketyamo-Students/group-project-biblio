3a4b5d75abd67da684f2b4d67d10f2aca689ac91
exo <setane122@gmail.com>
2025-07-10 10:26:52 +0100

    usercreated

--- controlles/User.controller.ts
+++ controlles/User.controller.ts
@@ -1,5 +1,44 @@
+import { Request, Response } from "express";
+import { User } from "../generated/prisma";
+import schema from "../utils/validator";
+import bcrypt from "bcrypt";
+import prisma from "../utils/prisma";
 const UserController = {
-  userSignUp: async () => {},
+  userSignUp: async (req: Request, res: Response) => {
+    try {
+      const { name, email, password }: User = req.body;
+      const { error } = schema.validate({ name, email, password });
+      if (error) {
+        res.status(401).json({ msg: error.message });
+      } else {
+        const emailCheck = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (emailCheck) {
+          res.status(401).json({
+            msg: "user email exist",
+          });
+        } else {
+          const pasEncrypted = `${bcrypt.hash(password, 10)}`;
+          const user = await prisma.user.create({
+            data: {
+              name,
+              email,
+              password: pasEncrypted,
+            },
+          });
+          res.status(201).json({
+            msg: "user created",
+            data: user,
+          });
+        }
+      }
+    } catch (error) {
+      res.status(500).json({ msg: "server error try latter" });
+    }
+  },
   userLogin: async () => {},
   userUpdate: async () => {},
   userDelete: async () => {},
