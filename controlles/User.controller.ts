11327d9b3f5ef7c0f273c91fb88ef1da26659540
exo <setane122@gmail.com>
2025-07-10 08:41:50 +0100

    UserConteroller

---
+++ controlles/User.controller.ts
@@ -0,0 +1,10 @@
+const UserController = {
+  userSignUp: async () => {},
+  userLogin: async () => {},
+  userUpdate: async () => {},
+  userDelete: async () => {},
+  userLogout: async () => {},
+  userProfile: async () => {},
+};
+
+export default UserController;
--- routes/Users.routes.ts
+++ routes/Users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
-
+import UserController from "../controlles/User.controller";
 const userRoutes = Router();
 
-userRoutes.get("/sigup", () => {});
+userRoutes.post("/sigup", UserController.userSignUp);
+userRoutes.post("/login", UserController.userLogin);
+userRoutes.get("/profile", UserController.userProfile);
+userRoutes.put("/update", UserController.userUpdate);
+userRoutes.delete("/sigup", UserController.userDelete);
 
 export default userRoutes;
