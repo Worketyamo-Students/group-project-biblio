b79d252f8f5329c076c45a580f1f7167c7388aab
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-07-13 17:59:50 +0100

    checks-loan

--- controlles/Loan.controllers.ts
+++ controlles/Loan.controllers.ts
@@ -1,6 +1,7 @@
 import { Loan } from "../generated/prisma";
 import { Request, Response } from "express";
 import prisma from "../utils/prisma";
+import { title } from "process";
 
 interface LoanRequest {
     userId: string;
@@ -14,8 +15,33 @@ const LoanController = {
         try {
             const { userId, booksIds, back }: LoanRequest = req.body;
 
-            if (!userId || !booksIds || !back) {
+            if (!userId || !booksIds || !back || !Array.isArray(booksIds)) { 
                 res.status(400).json({ msg: "all fields are required" });
+            }
+
+            // check if user exist
+            const user = await prisma.user.findUnique({
+                where: { id: userId },
+            });
+            if (!user) {
+                return res.status(400).json({ msg: "user does not exist" });
+            }
+
+            // check if books exist
+            const books = await prisma.books.findMany({
+                where: { id: { in: booksIds } }, 
+            })
+            // check if all books exist
+            if (books.length !== booksIds.length) {
+                return res.status(400).json({ msg: "one or some books does not exist" });
+            }
+
+            // check if all books are not already loaned
+            const alreadyLoanedBooks = books.filter(book => book.loanId !== null);
+            if (alreadyLoanedBooks.length > 0) {
+                return res.status(400).json({ 
+                    msg: "one or some books are already loaned",
+                    books: alreadyLoanedBooks.map(book => ({ id: book.id, title: book.title }))});
             }
 
             const loan = await prisma.loan.create({
@@ -67,21 +93,56 @@ const LoanController = {
             const { LoanId } = req.params;
             const { booksIds, back }: LoanRequest = req.body;
 
-            if (!booksIds || !back) {
+            if (!booksIds || !back || !Array.isArray(booksIds)) {
                 res.status(400).json({ msg: "all fields are required" });
             }
 
-            const loan = await prisma.loan.update({
-                where: {
-                    id: LoanId,
-                },
+            // check if loan exist
+            const loan = await prisma.loan.findUnique({
+                where: { id: LoanId },
+                include: { book: true },
+            })
+
+            if (!loan) {
+                return res.status(400).json({ msg: "loan does not exist" });
+            }
+
+            const books = await prisma.books.findMany({
+                where: { id: { in: booksIds } },
+            })
+
+            if (books.length !== booksIds.length) {
+                return res.status(400).json({ msg: "one or some books does not exist" });
+            }
+
+            // check if all books are not already loaned
+            const alreadyLoanedBooks = books.filter(
+                (book) => book.loanId !== null && book.loanId !== LoanId
+            )
+
+            // check if all books are not already loaned
+            if (alreadyLoanedBooks.length > 0) {
+                return res.status(400).json({
+                    msg: "one or some books are already loaned",
+                    books: alreadyLoanedBooks.map((book) => ({
+                        id: book.id,
+                        title: book.title,
+                    })),
+                })
+            }
+
+            const updatedLoan = await prisma.loan.update({
+                where: { id: LoanId },
                 data: {
                     back,
                     book: {
-                        connect: booksIds.map((id: string) => ({ id })),                    }
+                        set: [], // remove all books
+                        connect: booksIds.map((id: string) => ({ id })), // add new books
+                    },
                 },
                 include: { book: true, User: true }
             });
+
             res.status(201).json({ msg: "loan updated", data: loan });
         } catch (error) {
             res.status(500).json({ msg: "server error try latter" });
@@ -91,12 +152,37 @@ const LoanController = {
     loanDelete: async (req: Request, res: Response) => { 
         try {
             const { LoanId } = req.params;
+
+            const loan = await prisma.loan.findUnique({
+                where: { id: LoanId },
+                include: { book: true },
+            })
+
+            if (!loan) {
+                return res.status(400).json({ msg: "loan does not exist" });
+            }
+
+            // update book loanId
+            if (loan.book && loan.book.length > 0) {
+                await prisma.books.updateMany({
+                    where: { 
+                        id: {
+                            in: loan.book.map((book) => book.id),
+                        },
+                    },
+                    data: {
+                        loanId: null,
+                    }
+                })
+            }
+
             const profile = await prisma.loan.delete({
                 where: {
                     id: LoanId,
                 },
             });
-            if (!profile) res.status(400).json({ msg: "Emprunt non найд" });
+            
+            if (!profile) res.status(400).json({ msg: "Emprunt non trouvé" });
             res.status(200).json({ msg: "Emprunt supprimé" });
         } catch (error) {
             res.status(500).json({ msg: "server error try latter" });
